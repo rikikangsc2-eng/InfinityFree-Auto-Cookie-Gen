f41be1d47144cd0be120b98e066c96094bab5ac4
Puruu Puruu <nueapi11@gmail.com>
2025-06-07 01:45:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
-    res.succesJson = (data, statusCode = 200) => res.status(statusCode).json({ author: "NirKyy", success: true, data: data });
-    res.successJson = (data, statusCode = 200) => res.status(statusCode).json({ published_By: "NirKyy", success: true, data: data });
-    res.errorJson = (message, statusCode = 500) => res.status(statusCode).json({ published_By: "NirKyy", success: false, status: statusCode, error: message });
+    res.successJson = (data, statusCode = 200) => res.status(statusCode).json({ author: "Ini punya NirKyy", success: true, data: data });
+    res.errorJson = (message, statusCode = 500) => res.status(statusCode).json({ author: "Gak tau ini kode punya siapa sih bapuk bet error", success: false, status: statusCode, error: message });
     next();
 });
 
